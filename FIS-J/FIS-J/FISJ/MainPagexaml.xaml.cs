e0a11e2de953f0b86c639b7791aba020d6930dd6
kojdai <210447037@ccmailg.meijo-u.ac.jp>
2022-02-28 22:15:20 +0900

    UI

--- FIS-J/FIS-J/FISJ/MainPagexaml.xaml.cs
+++ FIS-J/FIS-J/FISJ/MainPagexaml.xaml.cs
@@ -12,14 +12,14 @@ namespace FIS_J
 public partial class MainPagexaml : ContentPage
 {
     public MainPagexaml()
-    { 
-
-        // Submit.Clicked += Submit_Clicked;
+    {
+            InitializeComponent();
+            Submit.Clicked += Submit_Clicked;
     }
 
     private void Submit_Clicked(object sender, EventArgs e)
     {
-        //Navigation.PushAsync(new SubmitFormxaml());
+        Navigation.PushAsync(new FISJ.SubmitFormxaml());
     }
 }
 }
