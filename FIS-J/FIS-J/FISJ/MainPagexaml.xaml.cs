e34a958ee23b5cd42bfcad75c72ffe3f66a2a45b
kojdai <210447037@ccmailg.meijo-u.ac.jp>
2022-03-04 18:38:03 +0900

    画像を貼り付けれるようになりました！

--- FIS-J/FIS-J/FISJ/MainPagexaml.xaml.cs
+++ FIS-J/FIS-J/FISJ/MainPagexaml.xaml.cs
@@ -11,15 +11,21 @@ namespace FIS_J
 {	[XamlCompilation(XamlCompilationOptions.Compile)]
 public partial class MainPagexaml : ContentPage
 {
-    public MainPagexaml()
+        public MainPagexaml()
     {
             InitializeComponent();
             Submit.Clicked += Submit_Clicked;
+
     }
 
     private void Submit_Clicked(object sender, EventArgs e)
     {
         Navigation.PushAsync(new FISJ.SubmitFormxaml());
+        }
+
+        private void Image_SizeChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
