6de90293e664e71be0688ea83b4e3ea80a1f2442
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-03-08 00:19:56 +0900

    移動/回転操作を実装

--- FIS-J/FIS-J/Components/FlightComputerSim.cs
+++ FIS-J/FIS-J/Components/FlightComputerSim.cs
@@ -35,6 +35,26 @@ namespace FIS_J.Components
 			WidthRequest = FCS_Compass.RADIUS * 2,
 		};
 
+		readonly double OverGrid_MinTop = -FCS_TrueIndex.RADIUS;
+		readonly double OverGrid_MaxTop = FCS_TASArc.E6BHeight - FCS_TrueIndex.RADIUS;
+
+		Thickness OverGridMargin = new();
+		double _CompassRotation = 0;
+		double CompassRotation
+		{
+			get => _CompassRotation;
+			set => _CompassRotation = value % 360;
+		}
+
+		enum TranslateMode
+		{
+			None,
+			Move,
+			Rotate,
+		}
+
+		TranslateMode CurrentMode = TranslateMode.None;
+
 		public FlightComputerSim()
 		{
 			over_grid.Children.Add(TrueIndex);
@@ -42,6 +62,85 @@ namespace FIS_J.Components
 
 			base_grid.Children.Add(TASArc);
 			base_grid.Children.Add(over_grid);
+
+			TouchTracking.TouchEffect CompassRotationEffect = new();
+
+			CompassRotationEffect.TouchAction += (s, e) =>
+			{
+				if (e.Type == TouchTracking.TouchActionType.Pressed)
+				{
+					Point loc = e.Location;
+					double radius = Math.Sqrt(Math.Pow(FCS_TrueIndex.RADIUS - loc.X, 2) + Math.Pow(FCS_TrueIndex.RADIUS - loc.Y, 2));
+					CurrentMode = radius switch
+					{
+						<= FCS_TrueIndex.RADIUS and > (FCS_TrueIndex.RADIUS - FCS_TrueIndex.ARC_THICKNESS) => TranslateMode.Move,
+						<= FCS_Compass.RADIUS and > (FCS_Compass.RADIUS - FCS_Compass.ARC_THICKNESS) => TranslateMode.Rotate,
+						_ => TranslateMode.None,
+					};
+				}
+				else if (CurrentMode != TranslateMode.None)
+				{
+					switch (CurrentMode)
+					{
+						case TranslateMode.Move:
+							double newTopTmp = OverGridMargin.Top + (e.AbsoluteLocation.Y - e.LastAbsLocation.Y);
+
+							if (newTopTmp < OverGrid_MinTop)
+								newTopTmp = OverGrid_MinTop;
+							else if (newTopTmp > OverGrid_MaxTop)
+								newTopTmp = OverGrid_MaxTop;
+
+							OverGridMargin = new(0, newTopTmp, 0, 0);
+							break;
+
+						case TranslateMode.Rotate:
+							double x1 = e.LastLocation.X - FCS_TrueIndex.RADIUS;
+							double y1 = e.LastLocation.Y - FCS_TrueIndex.RADIUS;
+							double x2 = e.Location.X - FCS_TrueIndex.RADIUS;
+							double y2 = e.Location.Y - FCS_TrueIndex.RADIUS;
+
+							if ((x1 == 0 && x2 == 0) || (y1 == 0 && y2 == 0))
+								break;
+
+							double rad1 = 0;
+							double rad2 = 0;
+							if (x1 == 0)
+								rad1 = y1 > 0 ? Math.PI / 2 : Math.PI * 3 / 2;
+							else
+								rad1 = Math.Atan(y1 / x1);
+
+							if (x2 == 0)
+								rad2 = y2 > 0 ? Math.PI / 2 : Math.PI * 3 / 2;
+							else
+								rad2 = Math.Atan(y2 / x2);
+
+							CompassRotation += (rad2 - rad1) * 180 / Math.PI;
+							break;
+					}
+
+					switch (e.Type)
+					{
+						case TouchTracking.TouchActionType.Cancelled:
+						case TouchTracking.TouchActionType.Entered:
+						case TouchTracking.TouchActionType.Exited:
+						case TouchTracking.TouchActionType.Released:
+							CurrentMode = TranslateMode.None;
+							break;
+					}
+				}
+			};
+
+			Device.StartTimer(new(0, 0, 0, 0, 20), () =>
+					{
+						if (over_grid.Margin.Top != OverGridMargin.Top)
+							over_grid.Margin = OverGridMargin;
+
+						if (Compass.Rotation != CompassRotation)
+							Compass.Rotation = CompassRotation;
+						return true;
+					});
+
+			over_grid.Effects.Add(CompassRotationEffect);
 
 			Content = base_grid;
 		}
