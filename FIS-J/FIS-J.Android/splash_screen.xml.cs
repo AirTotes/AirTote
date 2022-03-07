e4344a58e61005d727308036de5a54456e549370
konakahayato <konakahayato@hotmail.com>
2022-03-07 22:08:17 +0900

    飛行計画書　変更点

---
+++ FIS-J/FIS-J.Android/splash_screen.xml.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FIS_J.Droid
+{
+    [Activity(Label = "Activity1")]
+    public class Activity1 : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+    }
+}
