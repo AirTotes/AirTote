e9bfb43eedcb11764477b0a41e547a9246a998b2
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-03-08 00:16:54 +0900

    Add TouchEffect
    Use From: https://github.com/xamarin/xamarin-forms-samples/tree/main/Effects/TouchTrackingEffect
    (Apache 2.0 License)

---
+++ FIS-J/FIS-J/TouchEffect.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TouchTracking
+{
+	public enum TouchActionType
+	{
+		Entered,
+		Pressed,
+		Moved,
+		Released,
+		Exited,
+		Cancelled
+	}
+	public delegate void TouchActionEventHandler(object sender, TouchActionEventArgs args);
+
+	public class TouchActionEventArgs : EventArgs
+	{
+		public TouchActionEventArgs(long id, TouchActionType type, Point location, bool isInContact, Point absoluteLocation)
+		{
+			Id = id;
+			Type = type;
+			Location = location;
+			IsInContact = isInContact;
+			AbsoluteLocation = absoluteLocation;
+		}
+
+		public long Id { private set; get; }
+
+		public TouchActionType Type { private set; get; }
+
+		public Point Location { private set; get; }
+		public Point LastLocation { set; get; }
+		public Point AbsoluteLocation { set; get; }
+		public Point LastAbsLocation { set; get; }
+
+		public bool IsInContact { private set; get; }
+	}
+
+	public class TouchEffect : RoutingEffect
+	{
+		public event TouchActionEventHandler TouchAction;
+
+		public TouchEffect() : base("XamarinDocs.TouchEffect")
+		{
+		}
+
+		public bool Capture { set; get; }
+		public Point LastLocation { private get; set; }
+		public Point LastAbsLocation { private get; set; }
+
+		public void OnTouchAction(Element element, TouchActionEventArgs args)
+		{
+			args.LastLocation = LastLocation;
+			args.LastAbsLocation = LastAbsLocation;
+			LastLocation = args.Location;
+			LastAbsLocation = args.AbsoluteLocation;
+			TouchAction?.Invoke(element, args);
+		}
+	}
+}
