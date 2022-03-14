f2c40d5afa2c2e5bd5eafd230f5fd485746591ab
konakahayato <konakahayato@hotmail.com>
2022-03-14 17:06:21 +0900

    GPSアクセス許可

--- FIS-J/FIS-J.Android/MainActivity.cs
+++ FIS-J/FIS-J.Android/MainActivity.cs
@@ -1,4 +1,5 @@
 ﻿
+using Android;
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
@@ -9,8 +10,37 @@ namespace FIS_J.Droid
 	[Activity(Label = "FIS_J", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
-		protected override void OnCreate(Bundle savedInstanceState)
+		const int RequestLocationId = 0;
+		readonly string[] LocationPermissions =
 		{
+	Manifest.Permission.AccessCoarseLocation,
+	Manifest.Permission.AccessFineLocation
+};
+
+		protected override void OnStart()
+		{
+			base.OnStart();
+			if ((int)Build.VERSION.SdkInt >= 23)
+			{
+				if (CheckSelfPermission(Manifest.Permission.AccessFineLocation) != Permission.Granted)
+				{
+					RequestPermissions(LocationPermissions, RequestLocationId);
+				}
+				else
+				{
+					// Permissions already granted - display a message.
+				}
+			}
+		}
+		
+
+
+		protected override void OnCreate(Bundle savedInstanceState)
+
+
+		{
+
+
 			base.OnCreate(savedInstanceState);
 			Xamarin.FormsMaps.Init(this, savedInstanceState);
 
