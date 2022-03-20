5cae365f137df981ef5877b596c7674d8ba77fd7
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-03-20 22:08:57 +0900

    eAIPの表示を試しに実装

--- FIS-J/FIS-J/Views/AboutPage.xaml.cs
+++ FIS-J/FIS-J/Views/AboutPage.xaml.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Threading.Tasks;
+using FIS_J.Services;
 using Xamarin.Forms;
 
 namespace FIS_J.Views
@@ -8,6 +10,18 @@ namespace FIS_J.Views
 		public AboutPage()
 		{
 			InitializeComponent();
+
+			Task.Run(async () =>
+			{
+				var ais = new AISJapan("your_id", "your_password");
+				System.Diagnostics.Debug.WriteLine("PASS Running");
+				var result = await ais.GetPage("https://aisjapan.mlit.go.jp/html/AIP/html/20220324/eAIP/20220324/JP-menu-jp-JP.html");
+				System.Diagnostics.Debug.WriteLine(result);
+				webView.Source = new HtmlWebViewSource()
+				{
+					Html = result
+				};
+			});
 		}
 	}
 }
