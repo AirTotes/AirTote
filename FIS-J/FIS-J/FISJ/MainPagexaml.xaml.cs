28ce6b479be4204fc289b9b054ee5d0f4a9d078c
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-03-20 18:02:33 +0900

    エラーを修正

--- FIS-J/FIS-J/FISJ/MainPagexaml.xaml.cs
+++ FIS-J/FIS-J/FISJ/MainPagexaml.xaml.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FIS_J.FISJ
-{	[XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPagexaml : ContentPage
-    {
-        public MainPagexaml()
-        {
-            InitializeComponent();
-        }
-    }
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPagexaml : ContentPage
+	{
+		public MainPagexaml()
+		{
+			InitializeComponent();
+		}
+	}
 }
