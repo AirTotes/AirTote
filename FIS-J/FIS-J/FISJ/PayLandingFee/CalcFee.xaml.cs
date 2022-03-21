2e94e39f30ca0099c1d89e8e9cd4fa697fb30448
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-03-22 03:24:43 +0900

    料金計算画面を実装

--- FIS-J/FIS-J/FISJ/PayLandingFee/CalcFee.xaml.cs
+++ FIS-J/FIS-J/FISJ/PayLandingFee/CalcFee.xaml.cs
@@ -1,4 +1,5 @@
-﻿using FIS_J.ViewModels.PayLandingFee;
+﻿using FIS_J.Services;
+using FIS_J.ViewModels.PayLandingFee;
 using Xamarin.Forms;
 
 namespace FIS_J.FISJ.PayLandingFee
@@ -11,6 +12,30 @@ namespace FIS_J.FISJ.PayLandingFee
 		{
 			InitializeComponent();
 			BindingContext = viewModel;
+
+			viewModel.Station = new()
+			{
+				icao = "RJTT"
+			};
+		}
+
+		public CalcFee(AVWX.Station station)
+		{
+			InitializeComponent();
+			viewModel.Station = station;
+			BindingContext = viewModel;
+		}
+
+		public CalcFee(CalcFeeViewModel viewModel)
+		{
+			InitializeComponent();
+			this.viewModel = viewModel;
+			BindingContext = viewModel;
+		}
+
+		async void Button_Clicked(object sender, System.EventArgs e)
+		{
+			await Navigation.PushAsync(new SelectAirport(viewModel));
 		}
 	}
 }
