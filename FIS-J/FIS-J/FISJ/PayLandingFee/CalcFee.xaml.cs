0f75e7c16f119771277b5529c42e0246bfd58bc0
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-03-21 20:03:38 +0900

    Add CalcFee Page

---
+++ FIS-J/FIS-J/FISJ/PayLandingFee/CalcFee.xaml.cs
@@ -0,0 +1,16 @@
+﻿using FIS_J.ViewModels.PayLandingFee;
+using Xamarin.Forms;
+
+namespace FIS_J.FISJ.PayLandingFee
+{
+	public partial class CalcFee : ContentPage
+	{
+		CalcFeeViewModel viewModel { get; } = new();
+
+		public CalcFee()
+		{
+			InitializeComponent();
+			BindingContext = viewModel;
+		}
+	}
+}
