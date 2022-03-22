6f36a001b57bacaac54608200b5b2a2a7b3ce602
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-03-23 02:00:38 +0900

    不要なusingを削除

--- FIS-J/FIS-J/FISJ/Metar.xaml.cs
+++ FIS-J/FIS-J/FISJ/Metar.xaml.cs
@@ -3,9 +3,6 @@ using FIS_J.Services;
 using FIS_J.ViewModels;
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
