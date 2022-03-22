37eabe42e4ea11f6cd46ee356e7b39a1cd81c6a1
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-03-23 01:54:15 +0900

    インデントをタブに変換

--- FIS-J/FIS-J/FISJ/Metar.xaml.cs
+++ FIS-J/FIS-J/FISJ/Metar.xaml.cs
@@ -13,29 +13,29 @@ using Xamarin.Forms.Xaml;
 
 namespace FIS_J.FISJ
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Metar : ContentPage
-    {
-        AVWX api { get; } = new AVWX("KQuqTZ1D1BfSsuXu4eN2lc3DnC46-tGsU-l023G6q0w");
-        MetarPageViewModel ViewModel { get; } = new MetarPageViewModel();
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Metar : ContentPage
+	{
+		AVWX api { get; } = new AVWX("KQuqTZ1D1BfSsuXu4eN2lc3DnC46-tGsU-l023G6q0w");
+		MetarPageViewModel ViewModel { get; } = new MetarPageViewModel();
 
-        public Metar()
-        {
-            InitializeComponent();
-            BindingContext = ViewModel;
+		public Metar()
+		{
+			InitializeComponent();
+			BindingContext = ViewModel;
 
-            Task.Run(async () =>
-            {
-                string _metar = await api.GetSanitizedMETAR(ICAOCode.RJAA);
-                System.Diagnostics.Debug.WriteLine($"METAR: {_metar}");
-                ViewModel.Metar = _metar;
-            });
-            Task.Run(async () =>
-            {
-                string _taf = await api.GetSanitizedTAF(ICAOCode.RJAA);
-                System.Diagnostics.Debug.WriteLine($"TAF: {_taf}");
-                ViewModel.taf = _taf;
-            });
-        }
-    }
+			Task.Run(async () =>
+			{
+				string _metar = await api.GetSanitizedMETAR(ICAOCode.RJAA);
+				System.Diagnostics.Debug.WriteLine($"METAR: {_metar}");
+				ViewModel.Metar = _metar;
+			});
+			Task.Run(async () =>
+			{
+				string _taf = await api.GetSanitizedTAF(ICAOCode.RJAA);
+				System.Diagnostics.Debug.WriteLine($"TAF: {_taf}");
+				ViewModel.taf = _taf;
+			});
+		}
+	}
 }
