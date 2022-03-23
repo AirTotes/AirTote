7e1e3efdacfbac867601eeab7634c1ebc04a3419
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-03-23 16:56:55 +0900

    インデントをタブに修正

--- FIS-J/FIS-J/FISJ/aero.xaml.cs
+++ FIS-J/FIS-J/FISJ/aero.xaml.cs
@@ -11,59 +11,59 @@ using FIS_J.ViewModels;
 
 namespace FIS_J.FISJ
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class aero : ContentPage
-    {
-        HtmlViewModel html = new();
-        public aero()
-        {
-            InitializeComponent();
-            BindingContext = html;
-            AICsView.Clicked += AICsView_Clicked;
-            AIPView.Clicked += AIPView_Clicked;
-            SUPsView.Clicked += SUPsView_Clicked;
-        }
-        private void SUPsView_Clicked(object sender, EventArgs e)
-        {
-            Task.Run(async () =>
-            {
-                var ais = new AISJapan("kojdai", "Kojdai0510");
-                System.Diagnostics.Debug.WriteLine("PASS Running");
-                var result = await ais.GetPage("https://aisjapan.mlit.go.jp/html/AIP/html/20220224/eSUP/JP-eSUPs-en-JP.html");
-                System.Diagnostics.Debug.WriteLine(result);
-                html.HTML = new HtmlWebViewSource()
-                {
-                    Html = result
-                };
-            });
-        }
-        private void AIPView_Clicked(object sender, EventArgs e)
-        {
-            Task.Run(async () =>
-            {
-                var ais = new AISJapan("kojdai", "Kojdai0510");
-                System.Diagnostics.Debug.WriteLine("PASS Running");
-                var result = await ais.GetPage("https://aisjapan.mlit.go.jp/html/AIP/html/20220224/eAIP/20220301/JP-menu-en-JP.html");
-                System.Diagnostics.Debug.WriteLine(result);
-                html.HTML = new HtmlWebViewSource()
-                {
-                    Html = result
-                };
-            });
-        }
-        private void AICsView_Clicked(object sender, EventArgs e)
-        {
-            Task.Run(async () =>
-            {
-                var ais = new AISJapan("kojdai", "Kojdai0510");
-                System.Diagnostics.Debug.WriteLine("PASS Running");
-                var result = await ais.GetPage("https://aisjapan.mlit.go.jp/html/AIP/html/20220324/eAIC/JP-eAICs-jp-JP.html");
-                System.Diagnostics.Debug.WriteLine(result);
-                html.HTML = new HtmlWebViewSource()
-                {
-                    Html = result
-                };
-            });
-        }
-    }
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class aero : ContentPage
+	{
+		HtmlViewModel html = new();
+		public aero()
+		{
+			InitializeComponent();
+			BindingContext = html;
+			AICsView.Clicked += AICsView_Clicked;
+			AIPView.Clicked += AIPView_Clicked;
+			SUPsView.Clicked += SUPsView_Clicked;
+		}
+		private void SUPsView_Clicked(object sender, EventArgs e)
+		{
+			Task.Run(async () =>
+			{
+				var ais = new AISJapan("kojdai", "Kojdai0510");
+				System.Diagnostics.Debug.WriteLine("PASS Running");
+				var result = await ais.GetPage("https://aisjapan.mlit.go.jp/html/AIP/html/20220224/eSUP/JP-eSUPs-en-JP.html");
+				System.Diagnostics.Debug.WriteLine(result);
+				html.HTML = new HtmlWebViewSource()
+				{
+					Html = result
+				};
+			});
+		}
+		private void AIPView_Clicked(object sender, EventArgs e)
+		{
+			Task.Run(async () =>
+			{
+				var ais = new AISJapan("kojdai", "Kojdai0510");
+				System.Diagnostics.Debug.WriteLine("PASS Running");
+				var result = await ais.GetPage("https://aisjapan.mlit.go.jp/html/AIP/html/20220224/eAIP/20220301/JP-menu-en-JP.html");
+				System.Diagnostics.Debug.WriteLine(result);
+				html.HTML = new HtmlWebViewSource()
+				{
+					Html = result
+				};
+			});
+		}
+		private void AICsView_Clicked(object sender, EventArgs e)
+		{
+			Task.Run(async () =>
+			{
+				var ais = new AISJapan("kojdai", "Kojdai0510");
+				System.Diagnostics.Debug.WriteLine("PASS Running");
+				var result = await ais.GetPage("https://aisjapan.mlit.go.jp/html/AIP/html/20220324/eAIC/JP-eAICs-jp-JP.html");
+				System.Diagnostics.Debug.WriteLine(result);
+				html.HTML = new HtmlWebViewSource()
+				{
+					Html = result
+				};
+			});
+		}
+	}
 }
