b83263afa28b76905005af2e807620510a460e7b
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-03-23 17:29:37 +0900

    Androidのアイコンを独自のものに変更

--- FIS-J/FIS-J.Android/MainActivity.cs
+++ FIS-J/FIS-J.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.Runtime;
 
 namespace FIS_J.Droid
 {
-	[Activity(Label = "FIS_J", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+	[Activity(Label = "FIS_J", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
 	{
 		protected override void OnCreate(Bundle savedInstanceState)
