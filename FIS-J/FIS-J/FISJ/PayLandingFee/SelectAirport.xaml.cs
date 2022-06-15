fd8e3552403776c71b0b963c4794fc18fb623873
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-06-16 07:27:08 +0900

    Mapsuiパッケージのupgrade対応、および緯度経度の線を描画する処理を追加
    緯度経度の線は、とりあえず単純に線が描画されるだけ。
    太さの変更やzoom levelによる表示/非表示は後ほど実装する

--- FIS-J/FIS-J/FISJ/PayLandingFee/SelectAirport.xaml.cs
+++ FIS-J/FIS-J/FISJ/PayLandingFee/SelectAirport.xaml.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 using FIS_J.Models;
 
-using Mapsui.Projection;
-using Mapsui.Rendering.Skia;
+using Mapsui.Extensions;
+using Mapsui.Layers;
+using Mapsui.Nts;
+using Mapsui.Nts.Extensions;
+using Mapsui.Projections;
+using Mapsui.Styles;
+using Mapsui.Tiling;
 using Mapsui.UI.Forms;
-using Mapsui.Utilities;
+
+using NetTopologySuite.Geometries;
 
 using Xamarin.Forms;
 
@@ -38,6 +44,7 @@ namespace FIS_J.FISJ.PayLandingFee
 
 			map = new();
 			map.Map.Layers.Add(OpenStreetMap.CreateTileLayer());
+			map.Map.Layers.Add(CreateLatLngLayer());
 			map.IsMyLocationButtonVisible = false;
 			map.MyLocationLayer.Enabled = false;
 			map.UnSnapRotationDegrees = 15;
@@ -50,7 +57,7 @@ namespace FIS_J.FISJ.PayLandingFee
 
 			var reso = map.Map.Resolutions[Math.Min(map.Map.Resolutions.Count - 1, 9)];
 
-			map.Map.Home = n => n.NavigateTo(SphericalMercator.FromLonLat(latlng.longitude, latlng.latitude), reso);
+			map.Map.Home = n => n.NavigateTo(SphericalMercator.FromLonLat(latlng.longitude, latlng.latitude).ToMPoint(), reso);
 
 			Appearing += SelectAirport_Appearing;
 			Disappearing += SelectAirport_Disappearing;
@@ -99,13 +106,13 @@ namespace FIS_J.FISJ.PayLandingFee
 					Svg = svg_str,
 				};
 
-				pin.Callout.Anchor = new Point(0, pin.Height * pin.Scale);
+				pin.Callout.Anchor = new(0, pin.Height * pin.Scale);
 				pin.Callout.RectRadius = 5;
 				pin.Callout.ArrowHeight = 8;
 				pin.Callout.ArrowWidth = 24;
 				pin.Callout.ArrowAlignment = ArrowAlignment.Top;
 				pin.Callout.ArrowPosition = 1;
-				pin.Callout.BackgroundColor = Color.White;
+				pin.Callout.BackgroundColor = Xamarin.Forms.Color.White;
 				pin.Callout.TitleFontSize = 16;
 				pin.Callout.SubtitleFontSize = 12;
 
@@ -153,7 +160,7 @@ namespace FIS_J.FISJ.PayLandingFee
 			{
 				pin.ShowCallout();
 				SelectedPin = pin;
-				map.Navigator.CenterOn(SphericalMercator.FromLonLat(apinfo.coordinates.longitude, apinfo.coordinates.latitude));
+				map.Navigator.CenterOn(SphericalMercator.FromLonLat(apinfo.coordinates.longitude, apinfo.coordinates.latitude).ToMPoint());
 				return;
 			}
 			else
@@ -168,6 +175,43 @@ namespace FIS_J.FISJ.PayLandingFee
 			_SelectedPin = SelectedPin;
 			SelectedPin?.HideCallout();
 			SelectedPin = null;
+		}
+
+		const int LAT_LINE_MAX = 86;
+		private static ILayer CreateLatLngLayer()
+		{
+			List<GeometryFeature> latlngLines = new();
+
+			for (int i = -180; i <= 180; i++)
+			{
+				Coordinate[] line = new[]
+				{
+					SphericalMercator.FromLonLat(i, -LAT_LINE_MAX).ToCoordinate(),
+					SphericalMercator.FromLonLat(i, LAT_LINE_MAX).ToCoordinate()
+				};
+				latlngLines.Add(new(new LineString(line)));
+			}
+			for (int i = -LAT_LINE_MAX; i <= LAT_LINE_MAX; i++)
+			{
+				Coordinate[] line = new[]
+				{
+					SphericalMercator.FromLonLat(-180, i).ToCoordinate(),
+					SphericalMercator.FromLonLat(180, i).ToCoordinate()
+				};
+				latlngLines.Add(new(new LineString(line)));
+			}
+
+			return new MemoryLayer()
+			{
+				Features = latlngLines,
+				Name = "LatLngLines",
+				Style = new VectorStyle()
+				{
+					Fill = null,
+					Outline = null,
+					Line = new(Mapsui.Styles.Color.FromArgb(180, 0, 0, 0))
+				}
+			};
 		}
 	}
 }
