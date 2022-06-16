8b449a026d0831f776a02aaac35feb15478da811
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-06-17 06:55:51 +0900

    不要な実装を削除

--- FIS-J/AppShell.xaml.cs
+++ FIS-J/AppShell.xaml.cs
@@ -10,17 +10,6 @@ namespace FIS_J
 		public AppShell()
 		{
 			InitializeComponent();
-			Routing.RegisterRoute(nameof(MainPagexaml), typeof(MainPagexaml));
-			Routing.RegisterRoute(nameof(SubmitReport), typeof(SubmitReport));
-			Routing.RegisterRoute(nameof(SkyInformation), typeof(SkyInformation));
-			Routing.RegisterRoute(nameof(WetherInformation), typeof(WetherInformation));
-			Routing.RegisterRoute(nameof(IcaoPage), typeof(IcaoPage));
-			Routing.RegisterRoute(nameof(SelectAirport), typeof(SelectAirport));
-			Routing.RegisterRoute(nameof(CalcFee), typeof(CalcFee));
-			Routing.RegisterRoute(nameof(SettingPage), typeof(SettingPage));
-			Routing.RegisterRoute(nameof(ReserveSpotAndFuel), typeof(ReserveSpotAndFuel));
-			Routing.RegisterRoute(nameof(Metar), typeof(Metar));
-			Routing.RegisterRoute(nameof(aero), typeof(aero));
 		}
 	}
 }
--- FIS-J/Platforms/Android/MainActivity.cs
+++ FIS-J/Platforms/Android/MainActivity.cs
@@ -1,11 +1,10 @@
-﻿
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
 
-namespace FIS_J
+namespace FIS_J;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
 {
-	[Activity(Label = "FIS-J", Icon = "@mipmap/appicon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
-	public class MainActivity : MauiAppCompatActivity
-	{
-	}
 }
