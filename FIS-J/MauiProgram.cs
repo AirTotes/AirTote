dda26ee4b2b201f9f342b9f5c70b49b37870eb35
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-06-26 23:29:31 +0900

    抜けていた半角スペースを追加
    一応、ファイル内の表記と合わせた

--- FIS-J/MauiProgram.cs
+++ FIS-J/MauiProgram.cs
@@ -13,7 +13,7 @@ public static class MauiProgram
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("BIZUDGothic-Bold.ttf", "BIZ UDGothic Bold");
-				fonts.AddFont("BIZUDGothic-Regular.ttf", "BIZUDGothic");
+				fonts.AddFont("BIZUDGothic-Regular.ttf", "BIZ UDGothic");
 				fonts.AddFont("BIZUDPGothic-Bold.ttf", "BIZ UDPGothic Bold");
 				fonts.AddFont("BIZUDPGothic-Regular.ttf", "BIZ UDPGothic");
 			});
