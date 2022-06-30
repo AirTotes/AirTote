a38dbb9b424501306d6b4c4009457173e3120113
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-06-30 21:48:23 +0900

    BIZ UDGothicのAliasについて、他と統一させた

--- FIS-J/FIS-J/AssemblyInfo.cs
+++ FIS-J/FIS-J/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: ExportFont("BIZUDGothic-Bold.ttf", Alias = "BIZ UDGothic Bold")]
-[assembly: ExportFont("BIZUDGothic-Regular.ttf", Alias = "BIZUDGothic")]
+[assembly: ExportFont("BIZUDGothic-Regular.ttf", Alias = "BIZ UDGothic")]
 [assembly: ExportFont("BIZUDPGothic-Bold.ttf", Alias = "BIZ UDPGothic Bold")]
 [assembly: ExportFont("BIZUDPGothic-Regular.ttf", Alias = "BIZ UDPGothic")]
 
