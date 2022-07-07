beb36cb7a3a55a3b93d0dce0c55d6ccd605ae8f5
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-08 00:05:04 +0900

    Create TopPage.cs
    新しいTopPage

---
+++ FIS-J/FISJ/TopPage.cs
@@ -0,0 +1,14 @@
+﻿using FIS_J.Maps;
+
+namespace FIS_J.FISJ;
+
+public class TopPage : ContentPage
+{
+	AirportMap Map { get; } = new();
+	public TopPage()
+	{
+		Content = Map;
+		Title = "Flight Information";
+	}
+}
+
