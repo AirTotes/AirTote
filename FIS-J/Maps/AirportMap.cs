5e62174aa47616b8bb73003d7a864c08d3c91a10
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-07 22:08:08 +0900

    Create AirportMap.cs
    空港所在地を表示するMap

---
+++ FIS-J/Maps/AirportMap.cs
@@ -0,0 +1,100 @@
+﻿using FIS_J.Models;
+using Mapsui.UI.Maui;
+
+namespace FIS_J.Maps;
+
+public record AirportSelectedEventArgs(AirportInfo.APInfo SelectedAP);
+
+public class AirportMap : AirMap
+{
+	const string AP_ICON_SVG = "MapIcons/flight.svg";
+
+	static Dictionary<string, AirportInfo.APInfo> StationsDic { get; set; } = null;
+
+	public event EventHandler<AirportSelectedEventArgs> AirportSelected;
+	public event EventHandler<AirportSelectedEventArgs> AirportClicked;
+
+	public AirportMap(Func<AirportInfo.APInfo, IEnumerable<CalloutText>, IEnumerable<CalloutText>> setCalloutText = null)
+	{
+		Init(setCalloutText);
+	}
+
+	public AirportMap(double longitude, double latitude, Func<AirportInfo.APInfo, IEnumerable<CalloutText>, IEnumerable<CalloutText>> setCalloutText = null)
+		: base(longitude, latitude)
+	{
+		Init(setCalloutText);
+	}
+
+	private async void Init(Func<AirportInfo.APInfo, IEnumerable<CalloutText>, IEnumerable<CalloutText>> setCalloutText)
+	{
+		Map.Layers.Add(LatLngLayerGenerator.Generate());
+		PinClicked += OnPinClicked;
+
+		StationsDic ??= await AirportInfo.getAPInfoDic();
+
+		Pins.Clear();
+
+		string svg_str = "";
+		using (var reader = new StreamReader(await FileSystem.OpenAppPackageFileAsync(AP_ICON_SVG)))
+			svg_str = await reader.ReadToEndAsync();
+
+		foreach (AirportInfo.APInfo ap in StationsDic.Values)
+		{
+			CustomTextCalloutPin pin = new(this)
+			{
+				Address = ap.name,
+				Label = ap.icao,
+				Type = PinType.Svg,
+
+				Position = new(ap.coordinates.latitude, ap.coordinates.longitude),
+
+				Scale = 0.5f,
+
+				Svg = svg_str,
+			};
+
+			pin.Callout.CalloutClicked += OnCalloutClicked;
+
+			using CalloutText baseText = new($"{ap.icao} ({ap.name})");
+			CalloutText[] baseTextArr = new[] { baseText };
+
+			IEnumerable<CalloutText> calloutTexts = setCalloutText?.Invoke(ap, baseTextArr) ?? baseTextArr;
+
+			pin.SetCalloutText(calloutTexts);
+
+			Pins.Add(pin);
+		}
+	}
+
+	private void OnPinClicked(object sender, PinClickedEventArgs e)
+	{
+		var pin = e.Pin;
+		if (pin is null)
+			return;
+
+		MoveTo(pin.Position);
+
+		e.Handled = true;
+
+		if (!StationsDic.TryGetValue(pin.Label, out AirportInfo.APInfo apinfo) || apinfo is null)
+			return;
+
+		if (pin.IsCalloutVisible())
+			// Callout表示中 <=> 既にPinがClickされている
+			AirportSelected?.Invoke(this, new(apinfo));
+		else
+		{
+			pin.ShowCallout();
+			AirportClicked?.Invoke(this, new(apinfo));
+		}
+	}
+
+	private void OnCalloutClicked(object sender, CalloutClickedEventArgs e)
+	{
+		e.Callout.Pin.HideCallout();
+		if (!StationsDic.TryGetValue(e.Callout.Pin.Label, out AirportInfo.APInfo apinfo) || apinfo is null)
+			return;
+
+		AirportSelected?.Invoke(this, new(apinfo));
+	}
+}
