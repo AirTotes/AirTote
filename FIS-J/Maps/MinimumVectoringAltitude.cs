349f4aad2433753e748b3c780bdb9341e7258772
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-10 20:40:01 +0900

    Create MinimumVectoringAltitude.cs

---
+++ FIS-J/Maps/MinimumVectoringAltitude.cs
@@ -0,0 +1,39 @@
+﻿using Mapsui.Layers;
+using Mapsui.Nts.Extensions;
+using Mapsui.Providers;
+using Mapsui.Styles;
+using NetTopologySuite.Geometries;
+using NetTopologySuite.IO;
+
+namespace FIS_J.Maps;
+
+public static class MinimumVectoringAltitude
+{
+	public static ILayer CreateLayer()
+	{
+		return new Layer("MinimumVectoringAltitude")
+		{
+			DataSource = new MemoryProvider(CreatePolygon().ToFeatures()),
+			Style = new VectorStyle
+			{
+				Fill = null,
+				Outline = new Pen
+				{
+					Color = Mapsui.Styles.Color.Black,
+					Width = 1,
+				}
+			}
+		};
+	}
+
+	private static List<Geometry> CreatePolygon()
+	{
+		var result = new List<Geometry>();
+
+		//var polygon = new WKTReader().Read("");
+		//result.Add(polygon);
+
+		return result;
+	}
+}
+
