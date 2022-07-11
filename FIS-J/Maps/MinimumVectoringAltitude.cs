4076908f542cad73b5209ec2eaf51c1dc8b43e1a
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-11 23:53:02 +0900

    リモートからのデータ読み込み機能を実装

--- FIS-J/Maps/MinimumVectoringAltitude.cs
+++ FIS-J/Maps/MinimumVectoringAltitude.cs
@@ -1,4 +1,5 @@
-﻿using Mapsui.Layers;
+﻿using FIS_J.Services;
+using Mapsui.Layers;
 using Mapsui.Nts.Extensions;
 using Mapsui.Providers;
 using Mapsui.Styles;
@@ -9,11 +10,13 @@ namespace FIS_J.Maps;
 
 public static class MinimumVectoringAltitude
 {
-	public static ILayer CreateLayer()
+	const string SERVER_URL = @"http://192.168.63.22:8080";
+
+	public static async Task<ILayer> CreateLayer()
 	{
 		return new Layer("MinimumVectoringAltitude")
 		{
-			DataSource = GetProvider(),
+			DataSource = await GetProvider(),
 			Style = new VectorStyle
 			{
 				Fill = null,
@@ -26,15 +29,30 @@ public static class MinimumVectoringAltitude
 		};
 	}
 
-	public static MemoryProvider GetProvider()
-		=> new(CreatePolygon().ToFeatures());
+	public static async Task<MemoryProvider> GetProvider()
+		=> new((await CreatePolygon()).ToFeatures());
 
-	private static List<Geometry> CreatePolygon()
+	static public bool? IsOffline { get; set; } = false;
+
+	public static async Task<List<Geometry>> CreatePolygon()
 	{
 		var result = new List<Geometry>();
 
-		//var polygon = new WKTReader().Read("");
-		//result.Add(polygon);
+		if (IsOffline is null)
+			return result;
+
+		try
+		{
+			using var stream = await (IsOffline == true ? FileSystem.OpenAppPackageFileAsync("RJTT.txt") : HttpService.HttpClient.GetStreamAsync(SERVER_URL + "/RJTT.txt"));
+
+			var polygon = new WKTReader().Read(stream);
+			result.Add(polygon);
+		}
+		catch
+		{
+			IsOffline = IsOffline == true ? null : true;
+			throw;
+		}
 
 		return result;
 	}
