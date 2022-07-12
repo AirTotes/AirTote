cf8bf7e52526b877286eb03908f4525dd8cbe25d
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-13 05:31:36 +0900

    MVA Textを表示する実装を追加
    Localから取得する実装は未実装

--- FIS-J/Maps/MinimumVectoringAltitude.cs
+++ FIS-J/Maps/MinimumVectoringAltitude.cs
@@ -1,8 +1,16 @@
-﻿using FIS_J.Services;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using FIS_J.Services;
+
+using Mapsui;
+using Mapsui.Extensions;
 using Mapsui.Layers;
 using Mapsui.Nts.Extensions;
+using Mapsui.Projections;
 using Mapsui.Providers;
 using Mapsui.Styles;
+
 using NetTopologySuite.Geometries;
 using NetTopologySuite.IO;
 
@@ -29,8 +37,19 @@ public static class MinimumVectoringAltitude
 		};
 	}
 
+	public static Layer CreateTextLayer()
+	{
+		return new Layer("MinimumVectoringAltitudeText")
+		{
+			Style = null
+		};
+	}
+
 	public static async Task<MemoryProvider> GetProvider(bool isLocal = false)
 		=> new((await (isLocal ? CreateLocalPolygon() : CreatePolygon())).ToFeatures());
+
+	public static async Task<MemoryProvider> GetTextProvider(bool isLocal = false)
+		=> new(await (isLocal ? CreateTexts() : CreateTexts()));
 
 	public static async Task<List<Geometry>> CreatePolygon()
 	{
@@ -54,6 +73,76 @@ public static class MinimumVectoringAltitude
 		result.Add(polygon);
 
 		return result;
+	}
+
+	class LabelTextsRecord
+	{
+		[JsonPropertyName("text")]
+		public string Text { get; set; }
+
+		[JsonPropertyName("lon")]
+		public double? Lon { get; set; }
+
+		[JsonPropertyName("lat")]
+		public double? Lat { get; set; }
+
+		[JsonPropertyName("x")]
+		public double? X { get; set; }
+
+		[JsonPropertyName("y")]
+		public double? Y { get; set; }
+
+		[JsonPropertyName("min-visible")]
+		public double? MinVisible { get; set; }
+
+		[JsonPropertyName("max-visible")]
+		public double? MaxVisible { get; set; }
+	}
+
+	public static async Task<List<IFeature>> CreateTexts()
+	{
+		List<IFeature> texts = new();
+
+		using var stream = await HttpService.HttpClient.GetStreamAsync(SERVER_URL + "/RJTT.text.json");
+
+		var result = await JsonSerializer.DeserializeAsync(stream, typeof(Dictionary<string, LabelTextsRecord[]>));
+
+		if (result is Dictionary<string, LabelTextsRecord[]> dic)
+		{
+			foreach (var arr in dic)
+				foreach (var v in arr.Value)
+					texts.Add(CreateLabelFeature(v));
+		}
+
+		return texts;
+	}
+
+	private static IFeature CreateLabelFeature(LabelTextsRecord d)
+	{
+		MPoint pt = new();
+
+		if (d.X is not null && d.Y is not null)
+		{
+			pt.X = d.X ?? 0;
+			pt.Y = d.Y ?? 0;
+		}
+		else if (d.Lon is not null && d.Lat is not null)
+			pt = SphericalMercator.FromLonLat(d.Lon ?? 0, d.Lat ?? 0).ToMPoint();
+		else
+			throw new ArgumentNullException("X-Y or Lon-Lat", "one of X-Y and Lon-Lat pairs must contain value");
+
+		return new PointFeature(pt)
+		{
+			Styles = new List<IStyle>()
+			{
+				new LabelStyle()
+				{
+					Text = d.Text,
+					MinVisible = d.MinVisible ?? 0,
+					MaxVisible = d.MaxVisible ?? double.MaxValue
+				}
+			}
+		};
 	}
 }
 
