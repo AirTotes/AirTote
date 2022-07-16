e5caddfd87415ab76d2dedd0965b56e7eaa8efed
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-17 06:02:38 +0900

    PaddingやMargin, Alignの設定機能を実装

--- FIS-J/Maps/InfoWidget.cs
+++ FIS-J/Maps/InfoWidget.cs
@@ -11,6 +11,16 @@ public class InfoWidget : Widget, INotifyPropertyChanged
 {
 	public event PropertyChangedEventHandler? PropertyChanged;
 
+	public float PaddingX { get; set; } = 4;
+	public float PaddingY { get; set; } = 4;
+
+	public InfoWidget()
+	{
+		// set default value
+		MarginX = 4;
+		MarginY = 4;
+	}
+
 	private MPoint _TapPos = new();
 	public MPoint TapPos
 	{
@@ -34,17 +44,17 @@ public class InfoWidgetRenderer : ISkiaWidgetRenderer, IDisposable
 {
 	const float TextSize = 12;
 	const float Margin = 4;
-	const float Padding = 4;
+
 	const string Header = "Resolution: ";
 	const string StrFormat = "#,###,###.##";
+
 	SKPaint textPaint { get; } = new() { Color = SKColors.Black, TextSize = 12 };
 	SKPaint bgPaint { get; } = new() { Color = SKColors.White, Style = SKPaintStyle.Fill };
-	SKRect rect { get; }
+	float textWidth { get; }
 
 	public InfoWidgetRenderer()
 	{
-		var width = textPaint.MeasureText(Header + StrFormat);
-		rect = new(Margin, Margin, Margin + Padding + width + Padding, Margin + Padding + TextSize + Padding);
+		textWidth = textPaint.MeasureText(Header + StrFormat);
 	}
 
 	public void Dispose()
@@ -58,9 +68,41 @@ public class InfoWidgetRenderer : ISkiaWidgetRenderer, IDisposable
 		if (_widget is not InfoWidget widget)
 			return;
 
+		var MarginX = widget.MarginX;
+		var MarginY = widget.MarginY;
+		var PaddingX = widget.PaddingX;
+		var PaddingY = widget.PaddingY;
+
+		SKRect rect = new(
+			left: widget.HorizontalAlignment switch
+			{
+				Mapsui.Widgets.HorizontalAlignment.Left => MarginX,
+				Mapsui.Widgets.HorizontalAlignment.Right => (float)viewport.Width - MarginX - PaddingX - textWidth - PaddingX,
+				_ => (float)viewport.CenterX - (textWidth / 2) - PaddingX
+			},
+			top: widget.VerticalAlignment switch
+			{
+				Mapsui.Widgets.VerticalAlignment.Top => MarginY,
+				Mapsui.Widgets.VerticalAlignment.Bottom => (float)viewport.Height - MarginY - PaddingY - TextSize - PaddingY,
+				_ => (float)viewport.CenterY - (TextSize / 2) - PaddingY
+			},
+			right: widget.HorizontalAlignment switch
+			{
+				Mapsui.Widgets.HorizontalAlignment.Left => MarginX + PaddingX + textWidth + PaddingX,
+				Mapsui.Widgets.HorizontalAlignment.Right => (float)viewport.Width - MarginX,
+				_ => (float)viewport.CenterX + (textWidth / 2) + PaddingX
+			},
+			bottom: widget.VerticalAlignment switch
+			{
+				Mapsui.Widgets.VerticalAlignment.Top => MarginY + PaddingY + TextSize + PaddingY,
+				Mapsui.Widgets.VerticalAlignment.Bottom => (float)viewport.Height - MarginY,
+				_ => (float)viewport.CenterY + (TextSize / 2) + PaddingY
+			}
+			);
+
 		canvas.DrawRect(rect, bgPaint);
 
 		string text = Header + viewport.Resolution.ToString(StrFormat);
-		canvas.DrawText(text, rect.Right - Padding - textPaint.MeasureText(text), rect.Top + Padding + TextSize, textPaint);
+		canvas.DrawText(text, rect.Right - PaddingX - textPaint.MeasureText(text), rect.Top + PaddingY + TextSize, textPaint);
 	}
 }
