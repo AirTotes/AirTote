45d3fd5ff5797a645f18a9c9d15deabcd2bf5d12
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-18 08:44:42 +0900

    Create FlyoutMenuItem.cs

---
+++ FIS-J/ViewModels/FlyoutMenuItem.cs
@@ -0,0 +1,17 @@
+﻿namespace FIS_J.ViewModels;
+
+public class FlyoutMenuItem : BaseViewModel
+{
+	public FlyoutMenuItem(Page page)
+	{
+		if (page is null)
+			throw new ArgumentNullException(nameof(page));
+
+		_Page = page;
+		Title = page.Title;
+	}
+
+	private Page _Page;
+	public Page Page => _Page;
+}
+
