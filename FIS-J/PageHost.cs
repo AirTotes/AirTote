0a82ac8633239f71c97b111df469c8598834e7de
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-18 09:14:21 +0900

    Create PageHost.cs
    Shellの代替

---
+++ FIS-J/PageHost.cs
@@ -0,0 +1,38 @@
+﻿using FIS_J.FISJ;
+using FIS_J.ViewModels;
+
+namespace FIS_J;
+
+public class PageHost : FlyoutPage
+{
+	TopPage topPage { get; }
+	FlyoutMenuPage MenuPage { get; }
+
+	public PageHost()
+	{
+		topPage = new();
+		MenuPage = new(topPage);
+
+		Flyout = MenuPage;
+
+		ChangeRootPage(topPage);
+		MenuPage.PageListView.SelectionChanged += PageListView_SelectionChanged;
+
+		// ref: https://github.com/dotnet/maui/issues/7520
+		FlyoutLayoutBehavior = FlyoutLayoutBehavior.Popover;
+	}
+
+	private void PageListView_SelectionChanged(object? sender, SelectionChangedEventArgs e)
+	{
+		if (e.CurrentSelection.FirstOrDefault() is not FlyoutMenuItem menuItem)
+			return;
+
+		ChangeRootPage(menuItem.Page);
+	}
+
+	void ChangeRootPage(Page page)
+	{
+		Detail = new NavigationPage(page);
+		IsPresented = false;
+	}
+}
