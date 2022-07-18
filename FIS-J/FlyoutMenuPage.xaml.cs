de616b370130aef82000240a36a9085277938b8c
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-18 09:14:10 +0900

    Create FlyoutMenuPage
    表示内容は概ねShell時代のものを踏襲している
    
    具体的なスタイルは面倒なので適用していない。

---
+++ FIS-J/FlyoutMenuPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.ObjectModel;
+
+using FIS_J.ViewModels;
+
+namespace FIS_J;
+
+public partial class FlyoutMenuPage : ContentPage
+{
+	ObservableCollection<FlyoutMenuItem> MenuItems { get; } = new();
+
+	public FlyoutMenuPage(Page topPage)
+	{
+		InitializeComponent();
+
+		WidthRequest = 300;
+
+		PageListView.ItemsSource = MenuItems;
+
+		Task.Run(() =>
+		{
+			MenuItems.Add(new(topPage));
+			MenuItems.Add(new(new FISJ.SubmitReport()));
+			MenuItems.Add(new(new FISJ.ReservePages.ReserveSpotAndFuel()));
+			MenuItems.Add(new(new FISJ.aero()));
+			MenuItems.Add(new(new FISJ.WetherInformation()));
+			MenuItems.Add(new(new FISJ.IcaoPage()));
+			MenuItems.Add(new(new FISJ.PayLandingFee.CalcFee()));
+			MenuItems.Add(new(new Views.SettingPage()));
+		});
+	}
+}
