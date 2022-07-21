6be052ac6ea2f46dd533cd67625ed345d698c37d
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-21 16:59:22 +0900

    RichTextKit (RichString) を用いてCalloutの文字を描画できる実装を追加

--- FIS-J/Maps/CustomTextCalloutPin.cs
+++ FIS-J/Maps/CustomTextCalloutPin.cs
@@ -3,10 +3,28 @@ using Mapsui.UI.Maui;
 
 using SkiaSharp;
 
+using Topten.RichTextKit;
+using IStyle = Topten.RichTextKit.IStyle;
+using Style = Topten.RichTextKit.Style;
+
 namespace FIS_J.Maps;
 
 public class CustomTextCalloutPin : Pin
 {
+	public static IStyle DefaultTextStyle { get; } = new Style()
+	{
+		BackgroundColor = SKColor.Empty,
+		FontFamily = "BIZ UDGothic",
+		FontSize = 16,
+		TextColor = SKColors.Black,
+	};
+
+	public TextPaintOptions TextPaintOptions { get; } = new()
+	{
+		IsAntialias = true,
+		LcdRenderText = true,
+	};
+
 	public CustomTextCalloutPin(MapView view) : base(view)
 	{
 		Callout.RectRadius = 5;
@@ -40,6 +58,52 @@ public class CustomTextCalloutPin : Pin
 			canvas.Clear();
 			foreach (var calloutText in texts)
 				calloutText.DrawTo(canvas);
+
+			using var wStream = new SKManagedWStream(memStream);
+			bitmap.Encode(wStream, SKEncodedImageFormat.Png, 100);
+		}
+
+		Callout.Content = BitmapRegistry.Instance.Register(memStream);
+		Callout.Type = CalloutType.Custom;
+	}
+
+	public void SetCalloutText(in IEnumerable<Func<RichString, RichString>> textGens)
+	{
+		RichString str = new();
+
+		foreach (var func in textGens)
+		{
+			if (func is not null)
+				str = func.Invoke(str);
+		}
+
+		SetCalloutText(str);
+	}
+
+	public void SetCalloutText(RichString richText)
+	{
+		if (Callout.Content > 0)
+			BitmapRegistry.Instance.Unregister(Callout.Content);
+		Callout.Content = -1;
+
+		richText.DefaultStyle = DefaultTextStyle;
+
+		var width = richText.MeasuredWidth;
+		if (width * 1.1 < richText.MaxWidth)
+		{
+			width *= 1.1f;
+			richText.MaxWidth = width;
+		}
+
+		MemoryStream memStream = new();
+		using (SKBitmap bitmap = new(
+			(int)width,
+			(int)richText.MeasuredHeight)
+		)
+		using (SKCanvas canvas = new(bitmap))
+		{
+			canvas.Clear();
+			richText.Paint(canvas, TextPaintOptions);
 
 			using var wStream = new SKManagedWStream(memStream);
 			bitmap.Encode(wStream, SKEncodedImageFormat.Png, 100);
