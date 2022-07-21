10de374640558eacf1e009f2016fb525e6cb27de
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-21 17:03:43 +0900

    RichTextKit採用に伴い不要になった実装を削除

--- FIS-J/Maps/CustomTextCalloutPin.cs
+++ FIS-J/Maps/CustomTextCalloutPin.cs
@@ -42,31 +42,6 @@ public class CustomTextCalloutPin : Pin
 		//Callout.Content = -1;
 	}
 
-	public void SetCalloutText(in IEnumerable<CalloutText> texts)
-	{
-		if (Callout.Content > 0)
-			BitmapRegistry.Instance.Unregister(Callout.Content);
-		Callout.Content = -1;
-
-		MemoryStream memStream = new();
-		using (SKBitmap bitmap = new(
-			(int)texts.Max(v => v.X + v.TextBounds.Width) + 8,
-			(int)texts.Max(v => v.TextBounds.Top + v.TextBounds.Height))
-		)
-		using (SKCanvas canvas = new(bitmap))
-		{
-			canvas.Clear();
-			foreach (var calloutText in texts)
-				calloutText.DrawTo(canvas);
-
-			using var wStream = new SKManagedWStream(memStream);
-			bitmap.Encode(wStream, SKEncodedImageFormat.Png, 100);
-		}
-
-		Callout.Content = BitmapRegistry.Instance.Register(memStream);
-		Callout.Type = CalloutType.Custom;
-	}
-
 	public void SetCalloutText(in IEnumerable<Func<RichString, RichString>> textGens)
 	{
 		RichString str = new();
