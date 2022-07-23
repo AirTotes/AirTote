574d3d66254fa2e3c8715404b71407c4c35c0303
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-07-23 12:38:03 +0900

    Install CommunityToolkit.Maui

--- FIS-J/MauiProgram.cs
+++ FIS-J/MauiProgram.cs
@@ -1,3 +1,5 @@
+using CommunityToolkit.Maui;
+
 using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace FIS_J;
@@ -10,6 +12,7 @@ public static class MauiProgram
 		builder
 			.UseMauiApp<App>()
 			.UseSkiaSharp()
+			.UseMauiCommunityToolkit()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("BIZUDGothic-Bold.ttf", "BIZ UDGothic Bold");
