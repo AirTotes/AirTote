6f6aac686d0a7aca9ba9ab8b3f25416571fe716e
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-08-10 03:13:41 +0900

    Mapだけ表示だとわかりにくかったので、上部にラベルを追加した

--- AirTote/Components/Maps/SelectAirport.cs
+++ AirTote/Components/Maps/SelectAirport.cs
@@ -22,6 +22,41 @@ public class SelectAirport : Popup
 
 		AirportMap map = new(latlng.longitude, latlng.latitude);
 
+		GradientStop gradientStopGen(double offset, byte alpha)
+		{
+			GradientStop ret = new(Colors.Transparent, (float)offset);
+
+			ret.SetAppThemeColor(
+				GradientStop.ColorProperty,
+				Colors.White.WithAlpha(alpha),
+				Colors.Black.WithAlpha(alpha)
+			);
+
+			return ret;
+		}
+
+		LinearGradientBrush labelBrush = new(
+			new()
+			{
+				gradientStopGen(0.0, 0),
+				gradientStopGen(0.2, 0xFF),
+				gradientStopGen(0.8, 0xFF),
+				gradientStopGen(1.0, 0),
+			},
+			new(0, 0),
+			new(1, 0)
+		);
+
+		Label label = new()
+		{
+			Text = "Please Select Airport",
+			VerticalOptions = LayoutOptions.Start,
+			HorizontalOptions = LayoutOptions.Center,
+			VerticalTextAlignment = TextAlignment.Center,
+			Background = labelBrush,
+			Padding = new(32, 4)
+		};
+
 		map.AirportSelected += (_, e) =>
 		{
 			if (airportInfo is not null)
@@ -30,7 +65,11 @@ public class SelectAirport : Popup
 			this.Close(e.SelectedAP);
 		};
 
-		Content = map;
+		Content = new Grid()
+		{
+			map,
+			label,
+		};
 	}
 }
 
