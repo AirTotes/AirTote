0d07850b75b68c46157e63b0c447b457b58ac84e
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-08-10 01:23:48 +0900

    Create FlyoutMenuPopup.cs

---
+++ AirTote/Components/FlyoutMenu/FlyoutMenuPopup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using CommunityToolkit.Maui.Views;
+
+namespace AirTote.Components.FlyoutMenu;
+
+public class FlyoutMenuPopup : Popup
+{
+	FlyoutMenuView MenuView { get; }
+
+	public FlyoutMenuPopup()
+	{
+		MenuView = new();
+
+		Content = MenuView;
+
+		MenuView.PageChangeRequested += (_, _) => this.Close();
+	}
+}
+
