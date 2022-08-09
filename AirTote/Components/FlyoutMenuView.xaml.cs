d887dee930a33dc1f4779c7123fe98e073fda62a
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-08-09 22:36:21 +0900

    FlyoutMenuをPopupで表示させるため、MenuPageをMenuViewに変更
    Shellへの変更にあわせて、ShellからMenuItemを引っ張ってくる方式に変更した

---
+++ AirTote/Components/FlyoutMenuView.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace AirTote.Components;
+
+public partial class FlyoutMenuView : ContentView
+{
+	public event EventHandler? PageChangeRequested;
+
+	public FlyoutMenuView()
+	{
+		InitializeComponent();
+
+		PageListView.SetBinding(CollectionView.ItemsSourceProperty, new Binding()
+		{
+			Source = Shell.Current,
+			Path = "Items",
+			Mode = BindingMode.OneWay,
+		});
+
+		PageListView.SelectionChanged += async (_, e) =>
+		{
+			if (e.CurrentSelection.Count <= 0 || e.CurrentSelection[0] is not ShellItem item)
+				return;
+
+			await (Shell.Current as IShellController).OnFlyoutItemSelectedAsync(item);
+			PageChangeRequested?.Invoke(this, new());
+		};
+	}
+}
