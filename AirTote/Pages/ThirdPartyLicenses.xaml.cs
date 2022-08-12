ef0b8ec57dbb5371997ff02f086d20b198aea415
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-08-12 16:13:09 +0900

    Add `ThirdPartyLicenses` Page

---
+++ AirTote/Pages/ThirdPartyLicenses.xaml.cs
@@ -0,0 +1,56 @@
+﻿using AirTote.Components;
+using AirTote.Models;
+
+namespace AirTote.Pages;
+
+public partial class ThirdPartyLicenses
+{
+	public const string LICENSE_INFO_DIR = "ThirdPartyLicenses";
+	public const string THIRD_PARTY_LICENSE_LIST_FILE_NAME = "third_party_license_list.json";
+
+	public ThirdPartyLicenses()
+	{
+		InitializeComponent();
+
+		PackageLicenseInfoView.Content = new Label()
+		{
+			Text = "Select package to check license"
+		};
+
+		Task.Run(Init);
+	}
+
+	async Task Init()
+	{
+		List<LicenseJsonSchema> licenseList = new();
+		await LoadJson(Path.Combine(LICENSE_INFO_DIR, THIRD_PARTY_LICENSE_LIST_FILE_NAME), licenseList);
+
+		licenseList.Sort((x, y) => string.Compare(x.id, y.id));
+
+		PackageListView.ItemsSource = licenseList;
+	}
+
+	static async Task LoadJson(string path, List<LicenseJsonSchema> licenses)
+	{
+		object? obj;
+		using (var stream = await FileSystem.OpenAppPackageFileAsync(path))
+		{
+			if (stream is null)
+				return;
+			obj = await System.Text.Json.JsonSerializer.DeserializeAsync(stream, typeof(LicenseJsonSchema[]));
+		}
+
+		if (obj is not LicenseJsonSchema[] arr)
+			return;
+
+		licenses.AddRange(arr);
+	}
+
+	void PackageListView_ItemTapped(object? sender, ItemTappedEventArgs e)
+	{
+		if (e.Item is not LicenseJsonSchema license)
+			return;
+
+		PackageLicenseInfoView.Content = new LicenseView(license);
+	}
+}
