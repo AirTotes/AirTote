f5f90944d243d612aae835806d952cf5b1c947fc
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-08-16 20:08:17 +0900

    不要なコードを削除

--- AirTote/ViewModels/SettingPageViewModel.cs
+++ AirTote/ViewModels/SettingPageViewModel.cs
@@ -1,19 +1,10 @@
-﻿using System;
-
-namespace AirTote.ViewModels
+﻿namespace AirTote.ViewModels
 {
 	public class SettingPageViewModel : BaseViewModel
 	{
 		public SettingPageViewModel()
 		{
 			Title = "Settings";
-		}
-
-		HtmlWebViewSource? _page = null;
-		public HtmlWebViewSource? WebPage
-		{
-			get => _page;
-			set => SetProperty(ref _page, value);
 		}
 	}
 }
