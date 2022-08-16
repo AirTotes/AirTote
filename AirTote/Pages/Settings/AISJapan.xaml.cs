d7f337de7ecd407df0342e10ae7eba9c190f8fe4
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-08-16 21:52:47 +0900

    AIS Japanのアカウント設定用ページを追加
    具体的な機能は未実装

---
+++ AirTote/Pages/Settings/AISJapan.xaml.cs
@@ -0,0 +1,17 @@
+﻿using AirTote.ViewModels.SettingPages;
+
+namespace AirTote.Pages.Settings;
+
+public partial class AISJapan : ContentPage
+{
+	public const string AIS_JAPAN_CREATE_ACCOUNT = "https://aisjapan.mlit.go.jp/ShowAccount.do";
+	public AISJapan()
+	{
+		InitializeComponent();
+
+		BindingContext = new AISJapanViewModel();
+	}
+
+	async void CreateAccount_Tapped(object sender, EventArgs e)
+		=> await Browser.OpenAsync(AIS_JAPAN_CREATE_ACCOUNT);
+}
