3e7ad654bbd7d6e55fac315c0eb53993036caad9
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-08-17 21:41:12 +0900

    Add `IsEnabled` Property
    資格情報ストアを使用できないプラットフォームで、アカウントの設定を無効化するため

--- AirTote/ViewModels/SettingPages/AISJapanViewModel.cs
+++ AirTote/ViewModels/SettingPages/AISJapanViewModel.cs
@@ -6,6 +6,9 @@ namespace AirTote.ViewModels.SettingPages;
 public partial class AISJapanViewModel : ObservableObject
 {
 	[ObservableProperty]
+	private bool _IsEnabled = false;
+
+	[ObservableProperty]
 	private string _Email = "";
 
 	[ObservableProperty]
