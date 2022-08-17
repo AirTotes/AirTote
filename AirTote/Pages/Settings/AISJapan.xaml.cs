ba8fd21517e898c475766372c7b3867e782ed46f
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-08-17 21:45:43 +0900

    Binding設定のミスを修正
    TwoPaneViewにはContentだけがホストされる仕様のため。
    ついでに、ViewModelをプロパティとして保持するようにした

--- AirTote/Pages/Settings/AISJapan.xaml.cs
+++ AirTote/Pages/Settings/AISJapan.xaml.cs
@@ -5,11 +5,14 @@ namespace AirTote.Pages.Settings;
 public partial class AISJapan : ContentPage
 {
 	public const string AIS_JAPAN_CREATE_ACCOUNT = "https://aisjapan.mlit.go.jp/ShowAccount.do";
+
+	AISJapanViewModel ViewModel { get; } = new();
+
 	public AISJapan()
 	{
 		InitializeComponent();
 
-		BindingContext = new AISJapanViewModel();
+		Content.BindingContext = ViewModel;
 	}
 
 	async void CreateAccount_Tapped(object sender, EventArgs e)
