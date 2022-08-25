a6e3dae5509c6c30993461f193c5892aecf56f60
kojdai <210447037@ccmailg.meijo-u.ac.jp>
2022-08-26 01:48:32 +0900

    ViewModelを整備

--- AirTote/Pages/TabChild/Imagery.xaml.cs
+++ AirTote/Pages/TabChild/Imagery.xaml.cs
@@ -1,3 +1,7 @@
+using AirTote.Services;
+using AirTote.TwoPaneView;
+using AirTote.ViewModels;
+
 namespace AirTote.Pages.TabChild;
 
 public partial class Imagery : ContentPage
@@ -5,5 +9,10 @@ public partial class Imagery : ContentPage
 	public Imagery()
 	{
 		InitializeComponent();
+
+		BindingContext = viewModel;
+		viewModel.ChangeRightPaneViewCommand = new(TPV);
 	}
+	//internal static readonly OpenBrowserCommand OpenUri = new();
+	ImageryPageViewModel viewModel { get; } = new();
 }
