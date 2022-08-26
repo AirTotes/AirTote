931dfe19d8e265df267dfef4cdf56ac4bdf5ed84
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-08-26 19:57:01 +0900

    RefleshInterval設定用にプロパティを追加

--- AirTote/ViewModels/SettingPages/TopPageSettingViewModel.cs
+++ AirTote/ViewModels/SettingPages/TopPageSettingViewModel.cs
@@ -7,11 +7,29 @@ public partial class TopPageSettingViewModel : ObservableObject
 {
 	static readonly TimeSpan IntervalDefaultValue = new(0, 0, 2);
 
+	static List<TimeSpan> _IntervalList { get; } = new List<TimeSpan>
+	{
+		new(0, 0, 0, 0, 100),
+		new(0, 0, 0, 0, 200),
+		new(0, 0, 0, 0, 500),
+		new(0, 0, 1),
+		new(0, 0, 2),
+		new(0, 0, 5),
+		new(0, 0, 10),
+		new(0, 0, 30),
+		new(0, 1, 0),
+	};
+
+	public static System.Collections.IList IntervalList => _IntervalList;
+	public static System.Collections.IList IntervalStrList => _IntervalList.Select(v => v.ToString(@"m\分s\.f\秒")).ToList();
+
 	public TopPageSettingViewModel()
 	{
 		PreferenceManager.TryGet<bool>(PreferenceManager.Keys.TopPage_EnableLocationService, ref _IsLocationEnabled);
 		PreferenceManager.TryGet<bool>(PreferenceManager.Keys.TopPage_EnableLocationFollowAnimation, ref _IsLocationFollowAnimationEnabled);
 		PreferenceManager.TryGet(PreferenceManager.Keys.TopPage_LocationRefleshInterval, ref _LocationRefleshInterval);
+
+		LocationRefleshIntervalIndex = _IntervalList.IndexOf(LocationRefleshInterval);
 	}
 
 	[ObservableProperty]
@@ -20,6 +38,11 @@ public partial class TopPageSettingViewModel : ObservableObject
 	private bool _IsLocationFollowAnimationEnabled = true;
 	[ObservableProperty]
 	private TimeSpan _LocationRefleshInterval = IntervalDefaultValue;
+	[ObservableProperty]
+	private int _LocationRefleshIntervalIndex;
+
+	partial void OnLocationRefleshIntervalIndexChanged(int value)
+		=> LocationRefleshInterval = _IntervalList[value];
 
 	partial void OnIsLocationEnabledChanged(bool value)
 		=> PreferenceManager.Set(PreferenceManager.Keys.TopPage_EnableLocationService, value);
