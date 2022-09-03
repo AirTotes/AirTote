294237dae9932544dca77f9784a31c2afea7ef6f
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-03 22:51:22 +0900

    動かせるファイルをServicesプロジェクトに移動

--- AirTote.Services/GetRemoteCsv.cs
+++ AirTote.Services/GetRemoteCsv.cs
@@ -1,5 +1,9 @@
 using Microsoft.VisualBasic.FileIO;
 
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 namespace AirTote.Services;
 
 public class GetRemoteCsv
