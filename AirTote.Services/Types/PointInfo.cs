b165a69e5ac5e12f3f742170698d24e5e6d44c43
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-04 08:02:15 +0900

    `PointInfo.HasLonLat`メソッドを追加

--- AirTote.Services/Types/PointInfo.cs
+++ AirTote.Services/Types/PointInfo.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace AirTote.Services.Types;
 
 public record PointInfo(
@@ -5,4 +7,10 @@ public record PointInfo(
 	string Name,
 	float? Latitude_Deg,
 	float? Longitude_Deg
-);
+)
+{
+	[MemberNotNullWhen(true, nameof(Latitude_Deg))]
+	[MemberNotNullWhen(true, nameof(Longitude_Deg))]
+	public bool HasLonLat()
+		=> Latitude_Deg is not null and not float.NaN && Longitude_Deg is not null and not float.NaN;
+}
