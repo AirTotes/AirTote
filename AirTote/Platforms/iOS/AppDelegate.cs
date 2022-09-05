1d9eee625e7efe606d22f7c66f3fc634e51ce24e
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-05 16:18:28 +0900

    Crashlyticsのセットアップコードを書いた

--- AirTote/Platforms/iOS/AppDelegate.cs
+++ AirTote/Platforms/iOS/AppDelegate.cs
@@ -1,10 +1,32 @@
 using Foundation;
 
+using UIKit;
+
 namespace AirTote;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
 	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+
+	public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+	{
+		bool result = base.FinishedLaunching(application, launchOptions);
+
+		try
+		{
+			Firebase.Core.App.Configure();
+			Firebase.Crashlytics.Crashlytics.SharedInstance.Init();
+			Firebase.Crashlytics.Crashlytics.SharedInstance.SetCrashlyticsCollectionEnabled(true);
+			Firebase.Crashlytics.Crashlytics.SharedInstance.SendUnsentReports();
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex);
+			Firebase.Crashlytics.Crashlytics.SharedInstance.Dispose();
+		}
+
+		return result;
+	}
 }
 
