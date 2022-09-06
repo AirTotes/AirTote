e14a463c6ab0e2d460f330324b6de198720edb33
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-07 00:40:58 +0900

    各点のアイコンが表示されるようにした

--- AirTote/Components/Maps/Layers/AirRouteLayer.PointObject.cs
+++ AirTote/Components/Maps/Layers/AirRouteLayer.PointObject.cs
@@ -1,3 +1,4 @@
+using AirTote.Services;
 using AirTote.Services.Types;
 
 using Mapsui.Extensions;
@@ -23,7 +24,10 @@ public partial class AirRouteLayer
 			MaxWidth = 240,
 			TitleFontColor = Mapsui.Styles.Color.Black,
 		};
-		VectorStyle IconStyle { get; } = new();
+		SymbolStyle IconStyle { get; } = new()
+		{
+			Fill = null,
+		};
 
 		public PointInfo PtInfo { get; }
 
@@ -36,6 +40,45 @@ public partial class AirRouteLayer
 			Styles.Add(this.CalloutStyle);
 
 			this.CalloutStyle.Title = ptInfo.Name;
+
+			Task.Run(SetIcon);
 		}
+
+		async void SetIcon()
+		{
+			int id = await GetPointIconId(PtInfo);
+
+			(IconStyle.SymbolType, IconStyle.BitmapId)
+				= id < 0
+				? (SymbolType.Ellipse, -1)
+				: (SymbolType.Image, id);
+		}
+
+		static Task<int> GetPointIconId(PointInfo ptInfo)
+		{
+			if (GetPointServiceType(ptInfo) is not string ptService || GetPointRepType(ptInfo) is not string ptRepType)
+				return Task.FromResult(-1);
+
+			return BitmapIdManager.GetSvgFromMauiAssetAsync($"MapIcons/{ptService}.{ptRepType}.svg");
+		}
+
+		static string? GetPointServiceType(PointInfo ptInfo)
+			=> ptInfo switch
+			{
+				PointInfo v when v.HasFlag(PointTypes.VOR) && v.HasFlag(PointTypes.DME) => "VORDME",
+				PointInfo v when v.HasFlag(PointTypes.VOR) && v.HasFlag(PointTypes.TACAN) => "VORTAC",
+				PointInfo v when v.HasFlag(PointTypes.VOR) => "VOR",
+				PointInfo v when v.HasFlag(PointTypes.TACAN) => "TACAN",
+				PointInfo v when v.HasFlag(PointTypes.CRP) || v.HasFlag(PointTypes.ORRP) => "REP",
+				_ => null
+			};
+
+		static string? GetPointRepType(PointInfo ptInfo)
+			=> ptInfo switch
+			{
+				PointInfo v when v.HasFlag(PointTypes.CRP) => "CRP",
+				PointInfo v when v.HasFlag(PointTypes.ORRP) => "ORRP",
+				_ => null
+			};
 	}
 }
