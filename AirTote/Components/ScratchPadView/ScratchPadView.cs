5873732270d08bdd59d1e508746975ff4cabf958
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-07 16:16:00 +0900

    `IsToolPickerVisible`プロパティを追加
    とりあえずVirtual側のみ実装。ハンドラ側は別で実装する

--- AirTote/Components/ScratchPadView/ScratchPadView.cs
+++ AirTote/Components/ScratchPadView/ScratchPadView.cs
@@ -5,9 +5,11 @@ namespace AirTote.Components;
 public interface IScratchPadView
 {
 	Page? HostPage { get; set; }
+	bool IsToolPickerVisible { get; set; }
 }
 
 [DependencyProperty<Page>("HostPage")]
+[DependencyProperty<bool>("IsToolPickerVisible")]
 public partial class ScratchPadView : View, IScratchPadView
 {
 }
