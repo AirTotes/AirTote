8eeca402cf94d919284419ef3c9696b77efef077
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-08 07:16:20 +0900

    Height / WidthをVirtualControlのものから更新するように設定

--- AirTote/Components/ScratchPadView/ScratchPadViewHandler.Android.cs
+++ AirTote/Components/ScratchPadView/ScratchPadViewHandler.Android.cs
@@ -11,4 +11,12 @@ public partial class ScratchPadViewHandler : ViewHandler<ScratchPadView, MauiScr
 
 	public static partial void MapIsToolPickerVisible(ScratchPadViewHandler handler, ScratchPadView scratchPad)
 		=> throw new NotImplementedException();
+	public static partial void MapCanvasHeight(ScratchPadViewHandler handler, ScratchPadView scratchPad)
+		=> throw new NotImplementedException();
+	public static partial void MapCanvasWidth(ScratchPadViewHandler handler, ScratchPadView scratchPad)
+		=> throw new NotImplementedException();
+	public static partial void MapHeight(ScratchPadViewHandler handler, ScratchPadView scratchPad)
+		=> throw new NotImplementedException();
+	public static partial void MapWidth(ScratchPadViewHandler handler, ScratchPadView scratchPad)
+		=> throw new NotImplementedException();
 }
