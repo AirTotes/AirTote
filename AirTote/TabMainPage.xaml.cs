def9eb69ded6f2822acbed157d24725eb03efe10
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-09 21:52:40 +0900

    Update TabMainPage.xaml.cs

--- AirTote/TabMainPage.xaml.cs
+++ AirTote/TabMainPage.xaml.cs
@@ -1,9 +1,24 @@
+using AirTote.Services;
+
 namespace AirTote;
 
 public partial class TabMainPage : Shell
 {
+	public Task<AISJapan> AISJapanConnection { get; } = AISJapanUtils.FromSecureStorageAsync(SecureStorage.Default);
+
+	private AISJapanParser.EAIP.Menu? _EAIPMenu;
+	public async Task<AISJapanParser.EAIP.Menu> GetEAIPMenuAsync()
+	{
+		if (_EAIPMenu is not null)
+			return _EAIPMenu;
+		else
+			return await AISJapanParser.EAIP.Menu.GetAsync(await AISJapanConnection, new(new(2022, 9, 8), new(2022, 9, 8)));
+	}
+
 	public TabMainPage()
 	{
 		InitializeComponent();
 	}
+
+	public static new TabMainPage? Current => Shell.Current as TabMainPage;
 }
