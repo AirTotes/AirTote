639a4d7973ea3ab2baeb3326aeb67af5ecdea726
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-09 22:46:54 +0900

    AIPDateを別のプロパティに移した

--- AirTote/TabMainPage.xaml.cs
+++ AirTote/TabMainPage.xaml.cs
@@ -1,3 +1,4 @@
+using AirTote.AISJapanParser.EAIP;
 using AirTote.Services;
 
 namespace AirTote;
@@ -6,13 +7,15 @@ public partial class TabMainPage : Shell
 {
 	public Task<AISJapan> AISJapanConnection { get; } = AISJapanUtils.FromSecureStorageAsync(SecureStorage.Default);
 
+	public AIPDateInfo AIPDate { get; } = new(new(2022, 9, 8), new(2022, 9, 8));
+
 	private AISJapanParser.EAIP.Menu? _EAIPMenu;
 	public async Task<AISJapanParser.EAIP.Menu> GetEAIPMenuAsync()
 	{
 		if (_EAIPMenu is not null)
 			return _EAIPMenu;
 		else
-			return await AISJapanParser.EAIP.Menu.GetAsync(await AISJapanConnection, new(new(2022, 9, 8), new(2022, 9, 8)));
+			return await AISJapanParser.EAIP.Menu.GetAsync(await AISJapanConnection, AIPDate);
 	}
 
 	public TabMainPage()
