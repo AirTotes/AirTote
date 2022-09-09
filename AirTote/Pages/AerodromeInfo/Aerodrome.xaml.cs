5d452e2e2f3a85607062edb0285fa5cddb7c6c9a
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2022-09-09 22:52:15 +0900

    eAIPの情報を表示できるようにした

--- AirTote/Pages/AerodromeInfo/Aerodrome.xaml.cs
+++ AirTote/Pages/AerodromeInfo/Aerodrome.xaml.cs
@@ -1,3 +1,4 @@
+using AirTote.AISJapanParser.EAIP;
 using AirTote.Models;
 using AirTote.ViewModels.AerodromeInfo;
 
@@ -9,6 +10,7 @@ namespace AirTote.Pages.AerodromeInfo;
 public partial class Aerodrome : ContentPage
 {
 	AerodromeViewModel VM { get; }
+	TabMainPage Host { get; }
 
 	public Aerodrome(AirportInfo.APInfo apInfo)
 	{
@@ -18,11 +20,31 @@ public partial class Aerodrome : ContentPage
 
 		BindingContext = VM;
 
-		Init();
+		this.APMap.Navigator?.NavigateTo(SphericalMercator.FromLonLat(VM.ApInfo.coordinates.longitude, VM.ApInfo.coordinates.latitude).ToMPoint(), 10);
+
+		Host = TabMainPage.Current ?? throw new Exception("TabMainPage.Current is null");
+
+		Task.Run(async () =>
+		{
+			if (TabMainPage.Current is null)
+				return;
+
+			var result = await TabMainPage.Current.GetEAIPMenuAsync();
+
+			VM.AIPItems = result.ItemList.OfType<MenuItem_Aerodrome>().Where(v => v.ICAO == VM.IcaoCode).ToList();
+		});
 	}
 
-	private void Init()
+	async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
 	{
-		this.APMap.Navigator?.NavigateTo(SphericalMercator.FromLonLat(VM.ApInfo.coordinates.longitude, VM.ApInfo.coordinates.latitude).ToMPoint(), 10);
+		if (e.Item is not MenuItem_Aerodrome item)
+			return;
+
+		string url = Host.AIPDate.GetUrl(item.FileName);
+		await Navigation.PushAsync(new WebViewPage(new()
+		{
+			BaseUrl = url,
+			Html = await (await Host.AISJapanConnection).GetPage(url)
+		}, item.ItemTitle));
 	}
 }
