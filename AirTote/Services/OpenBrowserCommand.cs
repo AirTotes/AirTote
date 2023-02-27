fbe2cad0cc8453f0015dbe639ab94a02d5ee34d5
ＴＲ <31824852+TetsuOtter@users.noreply.github.com>
2023-02-27 21:19:49 +0900

    エラーメッセージを拡充した
    - `string`でないときにErrMsg
    - `http://`や`https://`で始まらないときにErrMsg
    - ブラウザのopenに失敗したときにErrMsg

--- AirTote/Services/OpenBrowserCommand.cs
+++ AirTote/Services/OpenBrowserCommand.cs
@@ -10,20 +10,28 @@ internal class OpenBrowserCommand : ICommand
 
 	public bool CanExecute(object? parameter)
 	{
-		return parameter is not null;
+		return parameter is string;
 	}
 	public async void Execute(object? parameter)
 	{
 		if (parameter is string s)
 		{
+			if (!s.StartsWith("http://") && !s.StartsWith("https://"))
+			{
+				MsgBox.DisplayAlert("エラー", $"内部エラーです。開発者にお知らせください。\n({typeof(OpenBrowserCommand).FullName}: `{nameof(parameter)}` is not a valid url)", "OK");
+				return;
+			}
 			try
 			{
-				await Browser.Default.OpenAsync(s, BrowserLaunchMode.SystemPreferred);
+				if (!await Browser.Default.OpenAsync(s, BrowserLaunchMode.SystemPreferred))
+					MsgBox.DisplayAlert("エラー", $"ブラウザを開けませんでした。\nURL: {s} ", "OK");
 			}
 			catch (Exception ex)
 			{
 				MsgBox.DisplayAlert("エラー", $"ブラウザを開けませんでした {ex.Message}", "OK");
 			}
 		}
+		else
+			MsgBox.DisplayAlert("エラー", $"内部エラーです。開発者にお知らせください。\n({typeof(OpenBrowserCommand).FullName}: `{nameof(parameter)}` is not string)", "OK");
 	}
 }
